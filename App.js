92f20d46475395648757fee1cd48374cb6fa8f43
Liana Aruncutean <liana.aruncutean@gmail.com>
2022-05-22 08:23:47 +0300

    Added notification for specific events in app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,17 +7,49 @@ import BottomTabNavigator from "./navigation/TabNavigator";
 import FlashMessage from "react-native-flash-message";
 import CameraScreen from './screens/CameraScreen';
 import PreviewPhotoScreen from './screens/PreviewPhotoScreen';
-import HomeScreenAdmin from './screens/HomeScreenAdmin';
-import TenantsListScreen from './screens/TenantsListScreen';
-import TenantDetail from './screens/TenantDetail';
 import LandingScreen from './screens/LandingScreen';
 import AdminBottomTabNavigator from './navigation/AdminTabNavigator';
 import {LogBox} from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { Text, View, Button, Platform } from 'react-native';
+import { auth, db } from './firebase';
 
 const Stack = createNativeStackNavigator();
 LogBox.ignoreAllLogs(); 
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -27,20 +59,47 @@ export default function App() {
         <Stack.Screen options={ {headerShown: false} } name="PreviewPhotoScreen" component={PreviewPhotoScreen}/>
         <Stack.Screen options={ {headerShown: false} } name="Tab" component={BottomTabNavigator} />
         <Stack.Screen options={ {headerShown: false} } name="AdminTab" component={AdminBottomTabNavigator} />
-        {/* <Stack.Screen options={ {headerShown: false} } name="HomeAdmin" component={HomeScreenAdmin} />
-        <Stack.Screen options={ {headerShown: false} } name="TenantsList" component={TenantsListScreen} />
-        <Stack.Screen options={ {headerShown: false} } name="TenantDetail" component={TenantDetail} /> */}
       </Stack.Navigator>
       <FlashMessage position="top" />
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+async function registerForPushNotificationsAsync() {
+  let token;
+  const uid = auth.getUid();
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    if (uid && token) {
+      db
+        .collection('users')
+        .doc(uid)
+        .update({
+            notificationToken: token
+        });
+    }
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
--- utils.js
+++ utils.js
@@ -1,3 +1,65 @@
+import { auth, db } from "./firebase"
+import * as Notifications from 'expo-notifications';
+
 export function hasValue (variable) {
     return variable === undefined || variable === null ? false : true
+}
+
+export async function getTenantsUid () {
+  let tenantsUids = []
+  const querySnapshot = await db.collection('address').doc(global.currentAddress).collection('tenants').get()
+  querySnapshot.forEach((documentSnapshot) => {
+    tenantsUids.push(documentSnapshot.id)
+  })
+  return tenantsUids
+}
+
+async function getNotificationToken (uid) {
+    let notificationToken = ''
+    if (uid) {
+        const result = await db.collection('users').doc(uid).get()
+        notificationToken = result.data().notificationToken
+    }
+    return notificationToken
+} 
+
+export async function sendPushNotification (uid, title, body) {
+    let notificationToken = await getNotificationToken(uid)
+    console.log("from send push notif: " + notificationToken)
+    if (notificationToken && notificationToken !== '') {
+      let response = fetch('https://exp.host/--/api/v2/push/send', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Accept-Encoding': 'gzip, deflate',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            to: notificationToken,
+            data: { extraData: 'Some data' },
+            title: title,
+            body: body,
+          }),
+        })
+    }
+}
+
+export async function schedulePushNotification() {
+  console.log("aa")
+  const id = await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "APPartement",
+      body: "Perioada de transmitere a indexului a început!"
+    },
+    trigger: {
+      type: "calendar",
+      repeats: true,
+      dateComponents: {
+        day: 15,
+        hour: 17,
+        minute: 56,
+      }},
+  });
+  console.log("notif id on scheduling", id)
+  return id;
 }
